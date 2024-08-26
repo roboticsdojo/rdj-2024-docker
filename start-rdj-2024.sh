18aae208534ecbfc7172102f8b54138377b13655
codewithlennylen <codewithlennylen254@gmail.com>
2024-08-26 22:03:18 +0300

    Base Dockerfiles

---
+++ start-rdj-2024.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+CONTAINER_NAME="rdj-2024"
+
+# Function to check if container exists
+container_exists() {
+    sudo docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"
+}
+
+# Function to start existing container
+start_container() {
+    echo "Starting existing container ${CONTAINER_NAME}"
+    sudo docker start -ia ${CONTAINER_NAME}
+}
+
+
+# Check if the hardware is Raspberry Pi
+if grep -q "Raspberry Pi" /proc/cpuinfo; then
+    echo "Running on Raspberry Pi"
+
+    if container_exists; then
+        start_container
+    else
+        touch /tmp/.docker.xauth
+        xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
+
+        sudo docker run -it --name ${CONTAINER_NAME} --net=host --privileged \
+            --volume /tmp/.docker.xauth:/tmp/.docker.xauth \
+            --volume /tmp/.X11-unix:/tmp/.X11-unix \
+            --env DISPLAY=$DISPLAY \
+            --env XAUTHORITY=/tmp/.docker.xauth \
+            codewithlennylen/rdj-2024:latest
+    fi
+else
+    echo "Running on PC"
+    if container_exists; then
+        start_container
+    else
+        if [ -n "$WAYLAND_DISPLAY" ]; then
+            echo "Wayland detected, using Wayland socket"
+
+            sudo docker run -it --name ${CONTAINER_NAME} --net=host --privileged \
+                --volume $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
+                --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+                codewithlennylen/rdj-2024:latest
+        else
+            echo "X11 detected, using xauth"
+
+            XSOCK=/tmp/.X11-unix
+            XAUTH=/tmp/.docker.xauth
+            touch $XAUTH
+            xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+            sudo docker run -it --name ${CONTAINER_NAME} --net=host --privileged \
+                --volume $XSOCK:$XSOCK \
+                --volume $XAUTH:$XAUTH \
+                --env DISPLAY=$DISPLAY \
+                --env XAUTHORITY=$XAUTH \
+                codewithlennylen/rdj-2024:latest
+        fi
+    fi
+fi
